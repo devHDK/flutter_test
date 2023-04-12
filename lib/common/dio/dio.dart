096998194cf689524571e5b8400ac5268ab98cbc
devHDK <hyundo91082@gmail.com>
2023-04-12 21:23:31 +0900

    dio interceptor onError, onResponse

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -26,6 +26,67 @@ class CustomInterceptor extends Interceptor {
 
     return super.onRequest(options, handler);
   }
+
 // 2)응답을 받을때
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print(
+        '[RES][${response.requestOptions.method}] ${response.requestOptions.uri}');
+
+    return super.onResponse(response, handler);
+  }
+
 // 3)에러가 났을때
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    //401에러 (status code)
+    //토큰을 재발급 받는 시도를 하고 토큰이 재발급 되면
+    // 다시 새로운 토큰을 요청한다.
+    print('[ERROR][${err.requestOptions.method}] ${err.requestOptions.uri}');
+
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+
+    //refreshToken이 없으면
+    if (refreshToken == null) {
+      //에러를 던질때 handler.reject를 사용
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    if (isStatus401 && !isPathRefresh) {
+      final dio = Dio();
+      try {
+        final resp = await dio.post(
+          'http://$ip/auth/token',
+          options: Options(
+            headers: {
+              'authorization': 'Bearer $refreshToken',
+            },
+          ),
+        );
+
+        final accessToken = resp.data['accessToken'];
+        final options = err.requestOptions;
+
+        options.headers.addAll(
+          {
+            'authorization': 'Bearer $accessToken',
+          },
+        );
+
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+        //요청 재전송
+        final response = await dio.fetch(options);
+
+        //성공
+        return handler.resolve(response);
+      } on DioError catch (e) {
+        return handler.reject(e);
+      }
+    }
+    return handler.reject(err);
+  }
 }
